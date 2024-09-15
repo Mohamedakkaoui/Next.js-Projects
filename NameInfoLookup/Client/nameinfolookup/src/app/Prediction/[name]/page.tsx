c7c7bed6c54e70fce09c157cec315487dbd7852f
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-09-15 21:54:30 +0100

    Added functionality to predict and display age, gender, and country based on user input using external APIs

--- NameInfoLookup/Client/nameinfolookup/src/app/Prediction/[name]/page.tsx
+++ NameInfoLookup/Client/nameinfolookup/src/app/Prediction/[name]/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+const GetPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`);
+  return res.json();
+};
+const GetPredictedGender = async (Gender: string) => {
+  const res = await fetch(`https://api.genderize.io/?name=${Gender}`);
+  return res.json();
+};
+const GetPredictedCountry = async (Country: string) => {
+  const res = await fetch(`https://api.nationalize.io/?name=${Country}`);
+  return res.json();
+};
+interface Params {
+  params: { name: string };
+}
+async function page({ params }: Params) {
+  const ageData = GetPredictedAge(params.name);
+  const genderData = GetPredictedGender(params.name);
+  const countryData = GetPredictedCountry(params.name);
 
-function page({params} : any) {
+  const [age, gender, country] = await Promise.all([
+    ageData,
+    genderData,
+    countryData,
+  ]);
+
   return (
-    <div>{params.name}</div>
-  )
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="text-red-500 font-bold text-xl">Personal Info :</div>
+        <div>Age : {age?.age}</div>
+        <div>Gender : {gender?.gender}</div>
+        <div>Country : {country?.country[0]?.country_id}</div>
+      </div>
+    </>
+  );
 }
 
-export default page
+export default page;
